b360ebdcb75e181291b58d9480a3d491e03de636
Lok-ii <lokeshkataria.work@gmail.com>
2024-01-01 15:26:26 +0530

    progress bar completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [filled, setFilled] = useState(0);
+  const [loading, setLoading] = useState("Loading...");
+  const [color, setColor] = useState("black");
+
+  useEffect(()=>{
+    if(filled < 100){
+      setTimeout(() => {setFilled(oldValue => oldValue + 1)}, 100);
+    }
+
+    if(filled === 50){
+      setColor("white");
+    }
+
+    if(filled === 100){
+      setLoading("Completed!");
+    }
+  }, [filled]);
+
+  const onReload = ()=>{
+    setFilled(0);
+    setLoading("Loading...");
+    setColor("black");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer">
+        <p className="progressText">Progress Bar</p>
+        <div className="outerContainer">
+          <div className="innerContainer" style={{width: filled + "%"}}></div>
+          <p className="filled" style={{color: color}}>{filled}%</p>
+        </div>
+        <p className="status">{loading}</p>
+        <div className="buttons">
+          <button onClick={onReload}>Reload</button>
+        </div>
+      </div>
     </div>
   );
 }
